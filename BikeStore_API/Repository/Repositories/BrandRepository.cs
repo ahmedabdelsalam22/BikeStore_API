7718457200614c5ff0d550a586ed50ecdbc20e2b
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-08-30 02:22:39 +0300

    create BrandRepository

---
+++ BikeStore_API/Repository/Repositories/BrandRepository.cs
@@ -0,0 +1,12 @@
+﻿using BikeStore_API.Models;
+using BikeStore_API.Repository.IRepositories;
+
+namespace BikeStore_API.Repository.Repositories
+{
+    public class BrandRepository : Repository<Brand>, IBrandRepository
+    {
+        public BrandRepository(BikeStoresContext db) : base(db)
+        {
+        }
+    }
+}
