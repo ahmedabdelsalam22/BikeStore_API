bdee090d8aadafb463126bd365eb5308a24527d8
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-08-30 02:42:59 +0300

    add "BrandController" and inject IUnitOfWork to access Db

---
+++ BikeStore_API/Controllers/BrandController.cs
@@ -0,0 +1,18 @@
+﻿using BikeStore_API.Repository.UnitOfWork;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BikeStore_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public BrandController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+    }
+}
