65705429d20a85802fc1ed13eb3bcdf1be608ceb
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-08-30 01:43:31 +0300

    add Db Models

---
+++ BikeStore_API/Models/Part.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BikeStore_API.Models;
+
+public partial class Part
+{
+    public int PartId { get; set; }
+
+    public string? PartName { get; set; }
+}
---
+++ BikeStore_API/Models/ProductAudit.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BikeStore_API.Models;
+
+public partial class ProductAudit
+{
+    public int ChangeId { get; set; }
+
+    public int ProductId { get; set; }
+
+    public string ProductName { get; set; } = null!;
+
+    public int BrandId { get; set; }
+
+    public int CategoryId { get; set; }
+
+    public short ModelYear { get; set; }
+
+    public decimal ListPrice { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    public string Operation { get; set; } = null!;
+}
