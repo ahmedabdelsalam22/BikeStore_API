7f21d915f1021928cad72dd016d69cbe8248a4ae
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-08-30 02:00:19 +0300

    create IRepository

---
+++ BikeStore_API/Repository/IRepositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace BikeStore_API.Repository.IRepositories
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<List<T>> GetAll(Expression<Func<T,bool>>? filter = null);
+        Task<T> Get(Expression<Func<T,bool>>? filter=null);
+        void Create();
+        void Update();
+        void Delete();
+    }
+}
