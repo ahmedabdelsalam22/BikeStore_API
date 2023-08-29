37cad9545d6a763795a66d4cdb9f9f0cde7e2cf1
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-08-30 02:28:38 +0300

    create UnitOfWork and add IBrandRepository inject in it

---
+++ BikeStore_API/Repository/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿using BikeStore_API.Repository.IRepositories;
+
+namespace BikeStore_API.Repository.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        public IBrandRepository brandRepository { get; }
+    }
+}
