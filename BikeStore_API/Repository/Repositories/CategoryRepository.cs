e0ec2b5b55cfb1c2eed8495d22063bf0adabcc45
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-08-31 11:19:35 +0300

    create CategoryRepository

---
+++ BikeStore_API/Repository/Repositories/CategoryRepository.cs
@@ -0,0 +1,21 @@
+﻿using BikeStore_API.Models;
+using BikeStore_API.Repository.IRepositories;
+
+namespace BikeStore_API.Repository.Repositories
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly BikeStoresContext _db;
+        public CategoryRepository(BikeStoresContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Category category)
+        {
+            _db.Update(category);
+        }
+
+       
+    }
+}
