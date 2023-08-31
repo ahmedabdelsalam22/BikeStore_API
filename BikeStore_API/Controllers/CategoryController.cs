c9e22f42eb5f3ce637fdb1ec845c1572654a7de7
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-08-31 11:23:27 +0300

    create CategoryController and inject CategoryController and APIResponse in it

---
+++ BikeStore_API/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using BikeStore_API.Models;
+using BikeStore_API.Repository.UnitOfWork;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BikeStore_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly APIResponse _apiResponse;
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+            _apiResponse = new APIResponse();
+        }
+    }
+}
