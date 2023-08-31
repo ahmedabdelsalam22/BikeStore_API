3a6e8c9605f751e0238f825779069adb8386d62a
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-08-31 11:16:35 +0300

    create ICategoryRepository

---
+++ BikeStore_API/Repository/IRepositories/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using BikeStore_API.Models;
+
+namespace BikeStore_API.Repository.IRepositories
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category category);
+    }
+}
