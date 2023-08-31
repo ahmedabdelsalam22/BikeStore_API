04a3bc8220af8bc948350e37c4a03505189f3d63
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-08-31 13:55:35 +0300

    write logic for "CreateCustomer" and mapping CustomerCreateDTO to Customer

---
+++ BikeStore_API/DTOS/CustomerCreateDTO.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BikeStore_API.DTOS
+{
+    public class CustomerCreateDTO
+    {
+        [Required]
+        public string FirstName { get; set; } = null!;
+        [Required]
+        public string LastName { get; set; } = null!;
+
+        public string? Phone { get; set; }
+        [Required]
+        public string Email { get; set; } = null!;
+
+        public string? Street { get; set; }
+
+        public string? City { get; set; }
+
+        public string? State { get; set; }
+
+        public string? ZipCode { get; set; }
+    }
+}
