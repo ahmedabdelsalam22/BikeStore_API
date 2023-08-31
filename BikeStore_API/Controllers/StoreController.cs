9e088344383c9e72f4e3f7c981c2c9cf777a44db
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-08-31 19:04:32 +0300

    create StoreController and Inject IUnitOfWork,IMapper,APIResponse in it

---
+++ BikeStore_API/Controllers/StoreController.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using BikeStore_API.Models;
+using BikeStore_API.Repository.UnitOfWork;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BikeStore_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StoreController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        private readonly APIResponse _apiResponse;
+        public StoreController(IUnitOfWork unitOfWork,IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+            _apiResponse = new APIResponse();
+        }
+    }
+}
