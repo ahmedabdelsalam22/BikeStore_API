8e9c0004ec212aa9814e7e807c7259ff12834709
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-09-02 02:21:10 +0300

    fix issue in "GetProducts" by IgnoreCycles in Program cs

--- BikeStore_API/Models/Brand.cs
+++ BikeStore_API/Models/Brand.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace BikeStore_API.Models;
 
@@ -7,5 +8,6 @@ public partial class Brand
 {
     public int BrandId { get; set; }
     public string BrandName { get; set; } = null!;
-    public virtual ICollection<Product> Products { get; set; } = new List<Product>();
+    [JsonIgnore]
+    public virtual ICollection<Product>? Products { get; set; } = new List<Product>();
 }
